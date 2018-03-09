ec8f7a7a901f33c1da818cc0b7898a71c221e0a8
MinhDo2007 <minhdt.cntt@gmail.com>
2018-03-09 16:48:44 +0700

    test

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,14 @@ set :deploy_to, "/home/#{fetch(:user)}/#{fetch(:application)}"
 
 set :pty, true
 set :linked_files, %w{config/database.yml config/secrets.yml}
-set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system pubic}
-set :deploy_via, :remote_cache
+set :linked_dirs, fetch(:linked_dirs, [])
+  .push("log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads")
+# set :deploy_via, :remote_cache
 set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
